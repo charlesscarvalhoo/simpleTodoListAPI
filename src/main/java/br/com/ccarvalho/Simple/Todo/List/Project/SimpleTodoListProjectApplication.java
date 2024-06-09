8eadb4303e747e213f35d379a03a0c41dbd7d7c0
Charles Carvalho <charlesscarvalhoo@gmail.com>
2024-06-09 15:44:16 -0300

    creating project

---
+++ src/main/java/br/com/ccarvalho/Simple/Todo/List/Project/SimpleTodoListProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.ccarvalho.Simple.Todo.List.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTodoListProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ccarvalho/Simple/Todo/List/Project/SimpleTodoListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ccarvalho.Simple.Todo.List.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
