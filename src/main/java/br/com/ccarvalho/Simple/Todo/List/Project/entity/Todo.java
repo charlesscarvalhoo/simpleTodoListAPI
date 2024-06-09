b15c43c208974cca7c06301991a721c115534e2e
Charles Carvalho <charlesscarvalhoo@gmail.com>
2024-06-09 16:03:41 -0300

    model structuring

---
+++ src/main/java/br/com/ccarvalho/Simple/Todo/List/Project/entity/Todo.java
@@ -0,0 +1,66 @@
+package br.com.ccarvalho.Simple.Todo.List.Project.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="tb_todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean completed;
+    private int priority;
+
+    public Todo(){
+    }
+
+    public Todo(Long id, String name, String description, boolean completed, int priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.completed = completed;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
---
+++ src/main/java/br/com/ccarvalho/Simple/Todo/List/Project/service/TodoService.java
@@ -0,0 +1,38 @@
+package br.com.ccarvalho.Simple.Todo.List.Project.service;
+
+import br.com.ccarvalho.Simple.Todo.List.Project.entity.Todo;
+import br.com.ccarvalho.Simple.Todo.List.Project.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    TodoRepository todoRepository;
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sorted = Sort.by("priority").descending().and(
+                Sort.by("name").ascending());
+                return todoRepository.findAll(sorted);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
